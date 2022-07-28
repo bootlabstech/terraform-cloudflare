e2d8d8d4ea36a6553b7c6e5e390896cf41f14e92
aravindarc <aravind.kumar@bootlabstech.com>
2022-07-28 18:08:06 +0530

    moved auth out of provider block, to be supplied dynamically through env

--- variables.tf
+++ variables.tf
@@ -1,49 +1,9 @@
-variable "cloudflare_email" {
-  description = "The email associated with the account"
-  type        = string
-}
-
-variable "cloudflare_api_key" {
-  description = "The Cloudflare API key"
-  type        = string
-}
-
 variable "cloudflare_zone_id" {
   description = "Cloudflare Zone ID"
   type        = string
 }
 
-# variable "cloudflare_load_balancer_name" {
-#   description = "The DNS name (FQDN, including the zone) to associate with the load balancer."
-#   type        = string
-# }
-
-# variable "cloudflare_load_balancer_fallback_pool_id" {
-#   description = "The pool ID to use when all other pools are detected as unhealthy"
-#   type        = string
-# }
-
-# variable "cloudflare_load_balancer_default_pool_ids" {
-#   description = "A list of pool IDs ordered by their failover priority. Used whenever region/pop pools are not defined"
-#   type        = list(string)
-# }
-
-# variable "cloudflare_load_balancer_description" {
-#   description = "Cloudflare Load Balancer description"
-#   type        = string
-# }
-
-# variable "cloudflare_load_balancer_steering_policy" {
-#   description = "Determine which method the load balancer uses to determine the fastest route to your origin"
-#   type        = string
-#   default     = null
-#   validation {
-#     condition     = var.cloudflare_load_balancer_steering_policy == null || contains(["off", "geo", "dynamic_latency", "random", "proximity"], var.cloudflare_load_balancer_steering_policy == null ? "" : var.cloudflare_load_balancer_steering_policy)
-#     error_message = "The Cloudflare LB steering policy shall be one of the [off, geo, dynamic_latency, random, proximity]."
-#   }
-# }
-
-variable "cloudflare_record_name" {
+variable "cloudflare_name" {
   description = "Cloudflare record name"
   type        = string
 }
@@ -53,7 +13,7 @@ variable "cloudflare_value" {
   type        = string
 }
 
-variable "cloudflare_record_type" {
+variable "cloudflare_type" {
   description = "The type of the record"
   type        = string
 }
